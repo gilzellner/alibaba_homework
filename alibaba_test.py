81b1a64df17b4f778c018caa9c5844094dd5f942
Gil Zellner <gil.zellner@gmail.com>
2018-08-18 21:37:53 +0300

    master

--- alibaba_test.py
+++ alibaba_test.py
@@ -1,11 +1,15 @@
 from pylru import lrudecorator
 
+
+@lrudecorator(100)
+def get_log_lines(filename):
+    with open(filename) as f:
+        content = f.readlines()
+        return content
+
 @lrudecorator(100)
 def fetchLogs(namespace=None, numberOfLines=100, offset=0, reverse=False):
-    content =[]
-    # with open('/Users/gilzellner/Downloads/log_example.txt') as f:
-    with open('/Users/gilzellner/dev/alibaba_homework/test_data') as f:
-        content = f.readlines()
+    content = get_log_lines('/Users/gilzellner/Downloads/log_example.txt')
     if namespace is not None:
         content = [elem for elem in content if elem.startswith(namespace)]
     if offset:
@@ -21,9 +25,9 @@ def tail(namespace=None, numberOfLines=100):
     return fetchLogs(namespace=namespace, numberOfLines=numberOfLines, reverse=True)
 
 
-# print fetchLogs(numberOfLines=5, offset=0, reverse=True)
-# print fetchLogs(namespace='2018-08-12', numberOfLines=5, offset=0, reverse=False)
+print fetchLogs(numberOfLines=5, offset=0, reverse=True)
+print fetchLogs(namespace='2018-08-12', numberOfLines=5, offset=0, reverse=False)
 
 
-# print tail(namespace='', numberOfLines=5)
+print tail(namespace='', numberOfLines=5)
 
