a6d6d9820592e07396d42379b7af36b0f1b8c78c
Gil Zellner <gil.zellner@gmail.com>
2018-08-18 21:30:08 +0300

    master

--- alibaba_test.py
+++ alibaba_test.py
@@ -13,15 +13,16 @@ def fetchLogs(namespace=None, numberOfLines=100, offset=0, reverse=False):
     if numberOfLines > 100:
         raise(Exception('Number of lines limited to 100'))
     if reverse:
-        return content[-numberOfLines:]
+        content=content[-numberOfLines:]
+        return content[::-1]
     return content[:numberOfLines]
 
 def tail(namespace=None, numberOfLines=100):
     return fetchLogs(namespace=namespace, numberOfLines=numberOfLines, reverse=True)
 
 
-print fetchLogs(numberOfLines=5, offset=0, reverse=False)
-print fetchLogs(namespace='2018-08-12', numberOfLines=5, offset=0, reverse=False)
+print fetchLogs(numberOfLines=5, offset=0, reverse=True)
+# print fetchLogs(namespace='2018-08-12', numberOfLines=5, offset=0, reverse=False)
 
 
 # print tail(namespace='', numberOfLines=5)
